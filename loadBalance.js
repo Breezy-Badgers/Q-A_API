2a27a7064e9a4c4bea62f90efecaf62c25154ab8
Hoon Kang <hoonkang00@gmail.com>
2019-11-18 12:01:36 -0500

    a

---
+++ loadBalance.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+const port = 8080;
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+let count = 0;
+let server = ["18.222.190.178", "13.58.8.242"];
+
+app.get("/loaderio-899fec3d958ff6945cd4c4046c4a86d2", (req, res) => {
+  res.send("loaderio-899fec3d958ff6945cd4c4046c4a86d2");
+});
+
+app.get("/qa/:productId", (req, res) => {
+  res.redirect(`http://${server[count]}/qa/${req.params.productId}`);
+  if (count === 0) {
+    count = 1;
+  } else {
+    count = 0;
+  }
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
