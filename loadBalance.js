2cd3ece01be114777b040c1809b045b86eae9dfd
Hoon Kang <hoonkang00@gmail.com>
2019-11-18 14:16:22 -0500

    s

--- loadBalance.js
+++ loadBalance.js
@@ -12,18 +12,35 @@ app.use(cors());
 
 let count = 0;
 let server = ["18.222.190.178", "13.58.8.242"];
-
-app.get("/loaderio-899fec3d958ff6945cd4c4046c4a86d2", (req, res) => {
-  res.send("loaderio-899fec3d958ff6945cd4c4046c4a86d2");
-});
-
-app.get("/qa/:productId", (req, res) => {
-  res.redirect(`http://${server[count]}/qa/${req.params.productId}`);
+let toggle = () => {
   if (count === 0) {
     count = 1;
   } else {
     count = 0;
   }
+};
+app.get("/loaderio-899fec3d958ff6945cd4c4046c4a86d2", (req, res) => {
+  res.send("loaderio-899fec3d958ff6945cd4c4046c4a86d2");
+});
+
+app.get("/qa/:productId", (req, res) => {
+  res
+    .redirect(`http://${server[count]}/qa/${req.params.productId}`)
+    .then(toggle)
+    .catch(err => {
+      res.sendStatus(404);
+    });
+});
+
+app.put("/qa/question/:question_id/helpful", (req, res) => {
+  res
+    .redirect(
+      `http://${server[count]}/qa/question/${req.params.question_id}/helpful`
+    )
+    .then(toggle)
+    .catch(err => {
+      res.sendStatus(404);
+    });
 });
 
 app.listen(port, () => {
